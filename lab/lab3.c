7836b0f035b134a12fd128bd0ac1cd2673c6a56a
a-yew <jly4@sfu.ca>
2026-06-02 11:51:18 -0700

    Lab 3

--- lab/lab3.c
+++ lab/lab3.c
@@ -1,30 +1,92 @@
-#include <stddef.h>
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX_LEN 5
 
-int main() {
-  char *buff = NULL;
-  size_t size = 0;
-  char *s = "print";
-  char *arr[5] = {};
-  int i;
+typedef struct HistoryNode {
+  char *input;
+  struct HistoryNode *next;
+} HistoryNode;
+
+HistoryNode *head = NULL;
+HistoryNode *tail = NULL;
+int history_count = 0;
+
+char *get_input(void);
+void add_to_history(char *input);
+void remove_oldest_history(void);
+void print_history(void);
+void free_history(void);
+
+int main(void) {
   while (1) {
-    // recieve CLI input using getline()
-    printf("Enter input: ");
-    getline(&buff, &size, stdin);
-    buff[strcspn(buff, "\n")] = '\0';
-
-    if (strcmp(buff, s) == 0) {
-      for (i = 0; i < 5; i++) {
-        printf("%s\n", arr[i]);
-      }
+    char *input = get_input();
+    if (input == NULL) {
+      break;
+    }
+    if (strcmp(input, "print") == 0) {
+      print_history();
     }
   }
-  for (i = 0; i < 5; i++) {
-    free(arr[i]);
-  }
-  free(buff);
+  free_history();
   return 0;
+}
+
+char *get_input(void) {
+  char *line = NULL;
+  size_t n = 0;
+  printf("Enter input: ");
+  ssize_t len = getline(&line, &n, stdin);
+  if (len == -1) {
+    free(line);
+    return NULL;
+  }
+  if (len > 0 && line[len - 1] == '\n') {
+    line[len - 1] = '\0';
+  }
+  return line;
+}
+
+void remove_oldest_record(void) {
+  if (head == NULL) {
+    return;
+  }
+  HistoryNode *old_head = head;
+  head = head->next;
+  if (head == NULL) {
+    tail = NULL;
+  }
+  free(old_head->input);
+  free(old_head);
+  history_count--;
+}
+
+void add_to_history(char *input) {
+  HistoryNode *new_node = malloc(sizeof(HistoryNode));
+  if (new_node == NULL) {
+    perror("malloc error");
+    free(input);
+    exit(-1);
+  }
+  new_node->input = input;
+  new_node->next = NULL;
+  if (tail == NULL) {
+    head = new_node;
+    tail = new_node;
+  } else {
+    tail->next = new_node;
+    tail = new_node;
+  }
+  history_count++;
+  if (history_count > MAX_LEN) {
+    remove_oldest_record();
+  }
+}
+void print_history(void) {
+  HistoryNode *current = head;
+  while (current != NULL) {
+    printf("%s\n", current->input);
+    current = current->next;
+  }
 }
