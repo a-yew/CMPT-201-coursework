8896473fa8352fe6825222bf40467daa73a52a0c
a-yew <jly4@sfu.ca>
2026-06-09 15:23:19 -0700

    Lab 4

--- lab/lab4.c
+++ lab/lab4.c
@@ -11,6 +11,15 @@ struct header {
   struct header *next;
 };
 
+void print_out(char *format, void *data, size_t data_size) {
+  char buf[BUF_SIZE];
+  ssize_t len = snprintf(buf, BUF_SIZE, format,
+                         data_size == sizeof(uint64_t) ? *(uint64_t *)data : *(void **)data);
+
+  // if (len < 0) {handle_error("snprintf");
+  write(STDOUT_FILENO, buf, len);
+}
+
 int main() {
   // increase heap size
   void *ptr = sbrk(265);
@@ -33,15 +42,21 @@ int main() {
   memset(data2, 1, data_size);
 
   // print
+  print_out("first block: %p\n", &my_block, sizeof(my_block));
+  print_out("second block: %p\n", &my_block2, sizeof(my_block2));
+  print_out("first block size: %lu\n", &my_block->size, sizeof(uint64_t));
+  print_out("first block next: %p\n", &my_block->next, sizeof(my_block->next));
+  print_out("second block size: %lu\n", &my_block2->size, sizeof(uint64_t));
+  print_out("second block next: %p\n", &my_block2->next, sizeof(my_block2->next));
 
+  for (size_t i = 0; i < data_size; i++) {
+    uint64_t val = (uint8_t)data[i];
+    print_out("%lu\n", &val, sizeof(uint64_t));
+  }
+
+  for (size_t i = 0; i < data_size; i++) {
+    uint64_t val = (uint8_t)data2[i];
+    print_out("%lu\n", &val, sizeof(uint64_t));
+  }
   return 0;
-}
-
-void print_out(char *format, void *data, size_t data_size) {
-  char buf[BUF_SIZE];
-  ssize_t len = snprintf(buf, BUF_SIZE, format,
-                         data_size == sizeof(uint64_t) ? *(uint64_t *)data : *(void **)data);
-
-  // if (len < 0) {handle_error("snprintf");
-  write(STDOUT_FILENO, buf, len);
 }
